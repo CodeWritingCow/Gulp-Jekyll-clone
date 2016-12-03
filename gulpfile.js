dd8a4077c0bba9f258f7bc93125011efa075f121
Gary Pang <gary.pang@gmail.com>
2016-12-03 17:32:36 -0500

    Update gulpfile.js
    Create Gulp tasks "clean" and "concat." Add required plugins for them.
    "Clean" deletes old files from "build" folder. "Concat" concatenates CSS
    files into one main file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	clean = require('gulp-rimraf');
 
-gulp.task('default', [], function() {
-	console.log("Hello Gulp!");
+gulp.task('clean', [], function() {
+	console.log("Clean all files in build folder");
+
+	return gulp.src("build/*", { read: false }).pipe(clean());
+});
+
+gulp.task('default', ['clean'], function() {
+	console.log("Concatenating and moving all files in styles folder!");
+	return gulp.src("contents/styles/**.css")
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest("build/styles"));
 });
